80e611586f2b20050ce69243cc259752fdfda54c
Gregory Sigaud <gregs.atwork@gmail.com>
2021-03-04 18:05:38 +0000

    Removed and commented out some code

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
-import math
-import sys
-from os import rename
+# import math
+# import sys
+# from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
 def greet(who_to_greet):
@@ -13,8 +13,7 @@ def greet(who_to_greet):
     return greeting
 
 
-print(greet("World"))
-print(greet("Greg"))
+# print(greet("World"))
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
